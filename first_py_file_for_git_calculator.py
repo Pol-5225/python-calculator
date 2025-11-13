5aa63d0453f7ad9df4a7eba38442187d45f1e608
sangram <sangrampol5225@gmail.com>
2025-11-13 18:56:11 +0530

    commit No 2 Added a new function of multiplication

--- first_py_file_for_git_calculator.py
+++ first_py_file_for_git_calculator.py
@@ -10,3 +10,10 @@ if __name__ == "__main__":
     print("Calculator Started!")
     print(f"2 + 3 = {add(2, 3)}")
     print(f"5 - 2 = {subtract(5, 2)}")
+
+  # Adding new function for the ability of multiplication.
+    def multiply(a, b):
+        return a * b
+
+    # Add to your test section:
+    print(f"4 * 3 = {multiply(4, 3)}")
